1d92c2ef752a8cdd57c57c43ec10f892506c4f1a
Socorro Carrillo <mac_maria07@hotmail.com>
2015-04-21 12:19:05 -0500

    02. Post con ng-repeat

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,6 +1,13 @@
 console.log("#MariaGonzalez: Cargo app.js");
- var modulo1=angular.module("reeditgam", []);
+ var modulo1=
+ angular.module("reeditgam", []);
 modulo1.controller("mainCtrl",['$scope',
 	function ($scope){
-		$scope.test="Hola Angular";
+		$scope.test ="Hola Angular";
+		$scope.posts = ["Post 1 Hola",
+		"Post 2",
+		"Post 3",
+		"Post 4",
+		"Post 5",
+		"Post 6"];
 	} ]);
