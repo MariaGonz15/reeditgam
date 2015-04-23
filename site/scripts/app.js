84ebba9f7a314f1df519558815c32ef7e54e6912
Socorro Carrillo <mac_maria07@hotmail.com>
2015-04-23 11:49:07 -0500

    05.Se habilita la funcion de Post en forma automatica

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -5,6 +5,34 @@ modulo1.controller("mainCtrl",['$scope',
 	function ($scope){
 		$scope.test ="Hola Angular";
 
+		//modelo
+
+	$scope.posts = 
+[{title: "Post 1", upvotes: 5},
+{title: "Post 2", upvotes:15},
+{title: "Post 3", upvotes:7},
+{title: "Post 4", upvotes:9},
+{title: "Post 5", upvotes:3},
+{title: "Post 6", upvotes:5},
+];
+
+//metodo del controlador
+$scope.addPost = function (){
+if(!$scope.title || $scope.title === "")
+{
+alert ("No se permite postear titulos vacios")
+return;
+
+}
+
+$scope.posts.push(
+	{
+		title:$scope.title,upvotes: 0});
+ //two-way data binding 	
+$scope.title = "";
+ };
+
 
 
 }]);
+
