3051233288b26067d32c41e4b74a984a7d65ab97
Socorro Carrillo <mac_maria07@hotmail.com>
2015-05-07 12:59:20 -0500

    09.Se agrega un servicio de tipo factoory para almacenar los posts

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,20 +1,40 @@
 console.log("#MariaGonzalez: Cargo app.js");
  var modulo1=
  angular.module("reeditgam", []);
-modulo1.controller("mainCtrl",['$scope',
-	function ($scope){
+ //creando un servicio del tipo factory
+ modulo1.factory('posts',[function(){
+ 	//cuerpo del factory llamado post
+ 	var o = {
+ 		posts : [
+ 		{
+ 	    title: "Post 1", upvotes: 15,
+ 		comments: [
+ 		{author: "Karina", body:"Esto esta de pelos", upvotes:3},
+ 		{author: "Gamaliel",body:"Esto es basura" , upvotes:0}]
+ 	},
+ 	{
+ 	    title: "Post 2", upvotes: 4,
+ 		comments: [
+ 		{author: "Coco", body:"Esta asombroso", upvotes:5},
+ 		{author: "Cristian",body:"Esto esta aburrido" , upvotes:1}]
+ 	}
+ 		]
+ 	};
+ 	//retornando objeto de datos persistentes
+return o;
+ }]);
+
+ //creando controlador//
+
+modulo1.controller("mainCtrl",[
+	'$scope','posts',//inyectando factory post
+	function ($scope, posts){
 		$scope.test ="Hola Angular";
 
-		//modelo
+		//modelo al cual se le asigna 
+		//el resultado del factory
 
-	$scope.posts = 
-[{title: "Post 1", upvotes: 5},
-{title: "Post 2", upvotes:15},
-{title: "Post 3", upvotes:7},
-{title: "Post 4", upvotes:9},
-{title: "Post 5", upvotes:3},
-{title: "Post 6", upvotes:5},
-];
+	$scope.posts = posts.posts;
 
 //metodo del controlador
 $scope.addPost = function (){
